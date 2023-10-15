b7e2f2359d9440df2ce71ec16390706ecab02e77
flr22bbn <floraboban22@gmail.com>
2023-10-15 22:01:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [text, setText] = useState('');
+  const words = text.trim().split(/\s+/).filter(word => word !== '');
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Responsive Paragragh Word <br/>Counter</h1>
+      <div className="textarea-container">
+        <textarea
+          rows={Math.max(10, Math.ceil(words.length / 10))}
+          placeholder="Enter your text here..."
+          value={text}
+          onChange={handleTextChange}
+        />
+      </div>
+      <p>Word Count: {words.length}</p>
     </div>
   );
 }
-
 export default App;
